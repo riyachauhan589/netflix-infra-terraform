b497aa0f0b501c7857aa5c43a0b2f80904bd5113
riyachauhan589 <riyachauhan7521@gmail.com>
2026-02-14 17:33:37 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -9,6 +9,7 @@ resource "aws_vpc" "main_vpc" {
 resource "aws_subnet" "public_subnet" {
   vpc_id     = aws_vpc.main_vpc.id
   cidr_block = var.public_subnet_cidr
+ availability_zone = "ap-south-1a"
 
   tags = {
     Name = "netflix-public-subnet"
